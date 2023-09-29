54ed31e38f4766d02d5cf6b076a7f5445028f0cb
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2023-09-29 20:37:05 +0100

    Added content-type header to response

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,13 +3,16 @@
 """Index resource of api."""
 
 from json import dumps
+from flask import make_response
 from api.v1.views import app_views
 
 
 @app_views.route('/status')
 def status():
     """Checks if server is still up."""
-    return dumps({
+    response = make_response(dumps({
         'status': 'OK',
     },
-                 indent=2)
+                 indent=2))
+    response.headers['Content-Type'] = 'application/json'
+    return response
