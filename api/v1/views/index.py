226734560a577506242f1499190d3cb717d48cbe
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2023-09-29 21:15:54 +0100

    fixed some bugs

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,7 @@
 from json import dumps
 from flask import make_response
 from api.v1.views import app_views
+import models
 
 
 @app_views.route('/status')
@@ -14,5 +15,20 @@ def status():
         'status': 'OK',
     },
                  indent=2))
+    response.headers['Content-Type'] = 'application/json'
+    return response
+
+
+@app_views.route('/stats')
+def stats():
+    """Get some stats."""
+    data = {}
+    data['amenities'] = models.storage.count(models.amenity.Amenity)
+    data['cities'] = models.storage.count(models.city.City)
+    data['places'] = models.storage.count(models.place.Place)
+    data['reviews'] = models.storage.count(models.review.Review)
+    data['states'] = models.storage.count(models.state.State)
+    data['users'] = models.storage.count(models.user.User)
+    response = make_response(dumps(data, indent=2))
     response.headers['Content-Type'] = 'application/json'
     return response
